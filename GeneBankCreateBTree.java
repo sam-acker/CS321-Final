72afe784f1586bce6a65e56acc2360e5ff2945c5
CHRIS BENTLEY <christopherbentl@u.boisestate.edu>
2019-04-15 18:27:40 -0600

    Still adding files

--- GeneBankCreateBTree.java
+++ GeneBankCreateBTree.java
@@ -8,7 +8,7 @@ class GeneBankCreateBTree{
 	/**
 	
 	java GeneBankCreateBTree <0/1(no/with Cache)> <degree> <gbk file> <sequence length> [<debug level>]
-							  Args[0]  				Args[1]  Args[2]      Args[3]			Args[4]
+
 	*/	
 	public static void main(String... Args){
 		
--- GeneBankSearch.java
+++ GeneBankSearch.java
@@ -6,7 +6,7 @@ class GeneBankSearch{
 	/**
 	
 	java GeneBankSearch <0/1(no/with Cache)> <btree file> <query file> [<debug level>]
-						   Args[0]   			 Args[1]    Args[2]	     Args[3]
+	
 	*/
 	public static void main(String... Args){
 		
