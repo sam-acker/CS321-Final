e4a37e6f2d918c4ceeda7e2e3c1c6e17838a9b7c
CHRIS BENTLEY <christopherbentl@u.boisestate.edu>
2019-04-15 19:02:51 -0600

    Basic argument parsing

--- GeneBankCreateBTree.java
+++ GeneBankCreateBTree.java
@@ -12,6 +12,25 @@ class GeneBankCreateBTree{
 	*/	
 	public static void main(String... Args){
 		
+		//Parse input
+		try{
+		boolean useCache=false;
+		int degree=Integer.parseInt(Args[1]);
+		String gpkFileName=Args[2];
+		int debugLevel=0;
+		int seqLength=Integer.parseInt(Args[3]);
+		if (Args[0]=="1"){
+			useCache=true;
+		}
+		if (Args.length==5){
+			debugLevel=Integer.parseInt(Args[4]);
+		}
+		}catch(Exception e){
+			System.out.println("Error; format should be following:\njava GeneBankCreateBTree <0/1(no/with Cache)> <degree> <gbk file> <sequence length> [<debug level>]");
+			return;
+		}
+		//End parsing
+		
 		
 		
 		
--- GeneBankSearch.java
+++ GeneBankSearch.java
@@ -10,7 +10,23 @@ class GeneBankSearch{
 	*/
 	public static void main(String... Args){
 		
-		
+		//Parse input
+		try{
+		boolean useCache=false;
+		String btreeFile=Args[1];
+		String gpkFileName=Args[2];
+		int debugLevel=0;
+		if (Args[0]=="1"){
+			useCache=true;
+		}
+		if (Args.length==4){
+			debugLevel=Integer.parseInt(Args[3]);
+		}
+		}catch(Exception e){
+			System.out.println("Error; format should be following:\njava java GeneBankSearch <0/1(no/with Cache)> <btree file> <query file> [<debug level>]");
+			return;
+		}
+		//End parsing
 		
 		
 	}
