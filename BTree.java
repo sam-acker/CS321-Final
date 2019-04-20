d67adcf60c9662044c90aacbfdc77b00a71b4033
sam-acker <37353790+sam-acker@users.noreply.github.com>
2019-04-19 22:23:37 -0600

    Update BTree.java
    basic b tree

--- BTree.java
+++ BTree.java
@@ -1,21 +1,45 @@
 
 class BTree{
 	
-	int degree;
-	int byteOffset; //root
+	BTreeNode root; //root
 	
-	
-	
-	
-	class BTreeNode{
-		
-		
-		
-		
-		
-		
+	public BTree() {
+		root = null;
 	}
 	
+	//Node in tree
+	class BTreeNode{
+		public long key;	//base 4 binary DNA sequences of length k (1 <= k <= 31)
+		public BTreeNode left;
+		public BTreeNode right;
+
+		public BTreeNode(long key) {	
+			this.key = key;
+			left = null;
+			right = null;
+		}
+	}
+	
+	//Calls insertRecursive
+	void insert(long key) {
+		root = insertRecursive(root, key);
+	}
+	
+	//Recursive function to insert nodes to BTree
+	BTreeNode insertRecursive(BTreeNode root, long key) {
+		if(root == null) {
+			root = new BTreeNode(key);
+			return root;
+		}
+		
+		if(key < root.key) {
+			root.left = insertRecursive(root.left, key);
+		} else if(key > root.key) {
+			root.right = insertRecursive(root.right, key);
+
+		}
+		return root;
+	}
 	
 	
 	
