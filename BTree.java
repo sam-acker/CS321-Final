f00093ed5a2293437d360b483d42cdb607e50436
Chris <chris.bentley480@gmail.com>
2019-04-21 18:20:00 -0600

    basic btree struture

--- BTree.java
+++ BTree.java
@@ -1,74 +1,99 @@
-import java.util.LinkedList;
-import java.util.Queue;
 
+
+/**
+BTree class
+
+
+*/
 class BTree{
 	
-	int degree;
-	BTreeNode root; //Root of Tree
-	Queue<BTreeNode> queue = new LinkedList<BTreeNode>();	//Queue for Searching
 	
-	public BTree(int degree) {
-		this.degree = degree;
-		root = null;
-	}
 	
-	//Node in tree
+	
+	/**
+	
+	Node class
+	
+	
+	*/
+	
 	class BTreeNode{
-		public long key;	//base 4 binary DNA sequences of length k (1 <= k <= 31)
-		public BTreeNode left;
-		public BTreeNode right;
-
-		public BTreeNode(long key) {	
-			this.key = key;
-			left = null;
-			right = null;
-		}
-	}
-	
-	//Calls insertRecursive
-	void insert(long key) {
-		root = insertRecursive(root, key);
-	}
-	
-	//Recursive function to insert nodes to BTree
-	BTreeNode insertRecursive(BTreeNode root, long key) {
-		if(root == null) {
-			root = new BTreeNode(key);
-			return root;
-		}
 		
-		if(key < root.key) {
-			root.left = insertRecursive(root.left, key);
-		} else if(key > root.key) {
-			root.right = insertRecursive(root.right, key);
-
-		}
-		return root;
-	}
-	
-	//Returns number of times a specified DNA sequence occurs within the tree
-	int seqSearch(long key) {
-		if(root == null)
-			return -1;
 		
-		int retVal = 0;
+		/**
+		Constructor
 		
-		queue.clear();
-		queue.add(root);
-		while(!queue.isEmpty()) {
-			BTreeNode n = queue.remove();
+		*/
+		
+		public BTreeNode(){
 			
-			if(n.key == key) {
-				retVal++;
-			}
 			
-			if(n.left != null)
-				queue.add(n.left);
-			if(n.right != null)
-				queue.add(n.right);
+			
 		}
 		
-		return retVal;
+		/**
+		returns true if full, false if not full
+		*/
+		
+		public boolean full(){
+			return false;
+		}
+		
+		
+	}
+	
+	
+	
+	/**
+	Constructor
+	
+	
+	*/
+	public BTree(){
+		
+		
+		
+		
+	}
+	
+	/**
+	insert into BTree
+	
+	*/
+	public void insert(){
+		
+		
+		
+	}
+	
+	
+	/**
+	Nodes split when full
+	
+	*/
+	
+	public void split(){
+		
+		
+		
+	}
+	
+	/**
+	Search BTree
+	
+	*/
+	public void search(){
+		
+		
+	}
+	
+	/**
+	Print the tree to a txt file (if debug==1)
+	*/
+	public void dumpToTextfile(){
+		
+		
+		
 	}
 	
 	
