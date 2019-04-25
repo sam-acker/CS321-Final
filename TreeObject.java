3f57e106a5aee2d78916b596f38b88d99613ab70
Ben Kang <benkang@u.boisestate.edu>
2019-04-25 12:39:35 -0600

    Update TreeObj

--- TreeObject.java
+++ TreeObject.java
@@ -6,7 +6,7 @@ This class will be a single 'object' in a BTreeNode.
 @author Big ben
 */
 
-class TreeObject{
+class TreeObject implements Comparable<TreeObject> {
 	
 	private long key; //This is the sequence in long format
 	private int frequency; //This is the frequency of the key
@@ -27,9 +27,16 @@ class TreeObject{
 		return key;
 	}
 	
+	public void increaseFrequency() {
+		frequency ++;
+	}
+	
 	public int frequency() {
 		return frequency;
 	}
+	public int compareTo(TreeObject x) {
+		
+				}
 
 	
 }
